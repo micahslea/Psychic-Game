08752084decbaf2f3b87b35b4fe02c8a42c3f931
micahslea <micahslea@gmail.com>
2020-04-22 04:38:30 -0400

    added javascript. Game working but still needs some work. Needs to repeat the random letter until user guesses correctly or guesses incorrectly 9 times. I also want to display all of the user guesses at once.

---
+++ assets/javascript/game.js
@@ -0,0 +1,72 @@
+//ask the user to guess a letter - placed in HTML
+
+//Display wins, losses, and guesses left
+
+var computerChoices = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guess = "";
+var letter;
+
+//Display user's guesses
+
+//Generate a random letter by the computer
+
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guessesLeft-text");
+var guessesText = document.getElementById("guesses-text");
+
+document.onkeyup = function (event) {
+  var userGuess = event.key;
+
+  var computerGuess =
+    computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+  console.log(computerGuess);
+
+  if (userGuess == computerGuess) {
+    wins++;
+    guesses = 9;
+  } else if (userGuess !== computerGuess) {
+    guesses--;
+  }
+  if (guesses === 0) {
+    losses++;
+    guesses = 9;
+  }
+
+  winsText.textContent = "Wins: " + wins;
+  lossesText.textContent = "Losses: " + losses;
+  guessesLeftText.textContent = "Guesses Left: " + guesses;
+  guessesText.textContent = "Your Guesses So Far: " + userGuess;
+};
